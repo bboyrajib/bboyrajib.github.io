1a76ae23eff45e532f87dafbcda590baa4606b19
bboyrajib <bboyrajib@gmail.com>
2019-07-14 03:26:59 +0530

    finding falcone added

---
+++ finding-falcone/precache-manifest.5f52fa4c4a3434aecc657abdbb1dd850.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "226204dc097a1846ffeeedad9bdc1a07",
+    "url": "./index.html"
+  },
+  {
+    "revision": "eb2f5aa3cbb5d7561f01",
+    "url": "./static/css/main.4e85056b.chunk.css"
+  },
+  {
+    "revision": "578ef5d8211c071e2395",
+    "url": "./static/js/2.cbcafd2d.chunk.js"
+  },
+  {
+    "revision": "eb2f5aa3cbb5d7561f01",
+    "url": "./static/js/main.942c4dda.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
