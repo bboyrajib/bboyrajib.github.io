a2b35856ed02afadfbe1c8c87be3abf5080c736b
bboyrajib <bboyrajib@gmail.com>
2019-07-20 02:23:48 +0530

    homepage added for findinFalcone

---
+++ findingfalcone/precache-manifest.7eaf77a989e4585871c6e9f472b6e9c3.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11de3f280df1b18349e0a8e5ea6a63ba",
+    "url": "./index.html"
+  },
+  {
+    "revision": "77a2a6353987bfbc3a24",
+    "url": "./static/css/main.1f9d5eef.chunk.css"
+  },
+  {
+    "revision": "319cc304a33204b7dbca",
+    "url": "./static/js/2.9bbfb3f2.chunk.js"
+  },
+  {
+    "revision": "77a2a6353987bfbc3a24",
+    "url": "./static/js/main.0c8df95e.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "6513af62167762c8ccd9adea0fc94354",
+    "url": "./static/media/Donlon.6513af62.png"
+  },
+  {
+    "revision": "1401d00a24bcd706f548a81e8b068b7e",
+    "url": "./static/media/Enchai.1401d00a.png"
+  },
+  {
+    "revision": "d45dffae684707543a83ad57b7797b05",
+    "url": "./static/media/Jebing.d45dffae.png"
+  },
+  {
+    "revision": "be0b4004d69e3c8b777e5b26172fcb60",
+    "url": "./static/media/Lerbin.be0b4004.png"
+  },
+  {
+    "revision": "d98d786a561f1be06a95a686a0335ede",
+    "url": "./static/media/Pingasor.d98d786a.png"
+  },
+  {
+    "revision": "2bf5a0667c79191988f1481fab4a530c",
+    "url": "./static/media/Sapir.2bf5a066.png"
+  },
+  {
+    "revision": "c287dd041b572c5f9ecda004a9351b51",
+    "url": "./static/media/SpacePod.c287dd04.png"
+  },
+  {
+    "revision": "65bc3c0cb0847e8e035444f8cbcaa7c6",
+    "url": "./static/media/SpaceRocket.65bc3c0c.png"
+  },
+  {
+    "revision": "205da655b19c38f4a00da16574290221",
+    "url": "./static/media/SpaceShip.205da655.png"
+  },
+  {
+    "revision": "5b65990be7406f355aa7d5d8fa49191d",
+    "url": "./static/media/SpaceShuttle.5b65990b.png"
+  }
+]);
--- findingfalcone/service-worker.js
+++ findingfalcone/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.5ce03fc545d553fb81bc38c6afd53011.js"
+  "./precache-manifest.7eaf77a989e4585871c6e9f472b6e9c3.js"
 );
 
 self.addEventListener('message', (event) => {
